3bba246956599df11a6b5844b123a4a27cb67784
Karim Ahmed <devKarimAhmed@gmail.com>
2024-07-24 16:40:15 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CachedVideoPlayerController _videoPlayerController,
+      _videoPlayerController2,
+      _videoPlayerController3;
+
+  late CustomVideoPlayerController _customVideoPlayerController;
+  late CustomVideoPlayerWebController _customVideoPlayerWebController;
+
+  final CustomVideoPlayerSettings _customVideoPlayerSettings =
+      const CustomVideoPlayerSettings(showSeekButtons: true);
+
+  final CustomVideoPlayerWebSettings _customVideoPlayerWebSettings =
+      CustomVideoPlayerWebSettings(
+    src: longVideo,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    _videoPlayerController = CachedVideoPlayerController.network(
+      longVideo,
+    )..initialize().then((value) => setState(() {}));
+    _videoPlayerController2 = CachedVideoPlayerController.network(video240);
+    _videoPlayerController3 = CachedVideoPlayerController.network(video480);
+    _customVideoPlayerController = CustomVideoPlayerController(
+      context: context,
+      videoPlayerController: _videoPlayerController,
+      customVideoPlayerSettings: _customVideoPlayerSettings,
+      additionalVideoSources: {
+        "240p": _videoPlayerController2,
+        "480p": _videoPlayerController3,
+        "720p": _videoPlayerController,
+      },
+    );
+
+    _customVideoPlayerWebController = CustomVideoPlayerWebController(
+      webVideoPlayerSettings: _customVideoPlayerWebSettings,
+    );
+  }
+
+  @override
+  void dispose() {
+    _customVideoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CupertinoNavigationBar(
+        middle: Text("Appinio Video Player"),
+      ),
+      body: SafeArea(
+        child: ListView(
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            kIsWeb
+                ? Expanded(
+                    child: CustomVideoPlayerWeb(
+                      customVideoPlayerWebController:
+                          _customVideoPlayerWebController,
+                    ),
+                  )
+                : CustomVideoPlayer(
+                    customVideoPlayerController: _customVideoPlayerController,
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String longVideo =
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+String video720 =
+    "https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4";
+
+String video480 =
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
+
+String video240 =
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
